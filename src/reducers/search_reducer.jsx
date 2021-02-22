c37c79c8e4e715aa3beaaa7167d093faadbaa6c6
AnnaLupanova <lupanova.99@mail.ru>
2021-02-22 12:34:25 +0300

    finall sortBy

--- src/reducers/search_reducer.jsx
+++ src/reducers/search_reducer.jsx
@@ -1,4 +1,4 @@
-import {SEARCH_MOVIE, FETCH_MOVIES,SEARCH_MOVIES_BY_TITLE,SORT_MOVIES,IS_ACTIVE_SEARCH_BY,IS_ACTIVE_SORT_BY } from '../actions';
+import {SEARCH_MOVIE, FETCH_MOVIES,SEARCH_MOVIES_BY_TITLE,SORT_MOVIES } from '../actions';
 
 
 
@@ -6,9 +6,7 @@ const initialState = {
     text:'',
     movies:[],
     movie:[],
-    counter: 0,
-    isActiveSearchBy:false,
-    isActiveSortBy:false,
+    counter: 0
 }
 
 export default function (state=initialState,action){
@@ -18,36 +16,23 @@ export default function (state=initialState,action){
                 ...state,
                 text:action.payload
             }
-        case IS_ACTIVE_SEARCH_BY:
-            return{
-                ...state,
-                isActiveSearchBy:true,
-            }
-        case IS_ACTIVE_SORT_BY:
-            return{
-                ...state,
-                isActiveSortBy:true,
-            }
+
         case SEARCH_MOVIES_BY_TITLE:
             return{
                 ...state,
-                searchBy:action.payload,
+                searchBy:action.payload
             }
         case SORT_MOVIES:
-
             const sortedMovies = state.movies.map(a => a)
-
            return {
-                ...state,
-
-
-            }
+               movies: sortedMovies.sort((a,b)=>(b.vote_count-a.vote_count))
+               }
 
         case FETCH_MOVIES:
             return{
                 ...state,
                 movies:action.payload,
-
+                counter: action.payload.length-1,
             }
 
         default:
